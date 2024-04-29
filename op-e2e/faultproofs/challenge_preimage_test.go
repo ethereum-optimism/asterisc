45e46b87890fb14450373c45f2d0ea2be835c72f
pcw109550 <pcw109550@gmail.com>
2024-04-29 17:03:14 -0600

    op-e2e: Add asterisc fault proof e2e tests

---
+++ op-e2e/faultproofs/challenge_preimage_test.go
@@ -0,0 +1,69 @@
+package faultproofs
+
+import (
+	"context"
+	"testing"
+
+	"github.com/ethereum-optimism/asterisc/op-e2e/e2eutils/disputegame"
+	op_e2e "github.com/ethereum-optimism/optimism/op-e2e"
+	"github.com/ethereum-optimism/optimism/op-e2e/e2eutils/challenger"
+	"github.com/ethereum-optimism/optimism/op-e2e/e2eutils/disputegame/preimage"
+	op_e2e_faultproofs "github.com/ethereum-optimism/optimism/op-e2e/faultproofs"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/stretchr/testify/require"
+)
+
+func TestChallengeLargePreimages_ChallengeFirst(t *testing.T) {
+	op_e2e.InitParallel(t)
+	ctx := context.Background()
+	sys, _ := op_e2e_faultproofs.StartFaultDisputeSystem(t)
+	t.Cleanup(sys.Close)
+
+	disputeGameFactory := disputegame.NewAsteriscFactoryHelper(t, ctx, sys)
+	disputeGameFactory.StartChallenger(ctx, "Challenger",
+		challenger.WithAlphabet(sys.RollupEndpoint("sequencer")),
+		challenger.WithPrivKey(sys.Cfg.Secrets.Alice))
+	preimageHelper := disputeGameFactory.PreimageHelper(ctx)
+	ident := preimageHelper.UploadLargePreimage(ctx, preimage.MinPreimageSize,
+		preimage.WithReplacedCommitment(0, common.Hash{0xaa}))
+
+	require.NotEqual(t, ident.Claimant, common.Address{})
+
+	preimageHelper.WaitForChallenged(ctx, ident)
+}
+
+func TestChallengeLargePreimages_ChallengeMiddle(t *testing.T) {
+	op_e2e.InitParallel(t)
+	ctx := context.Background()
+	sys, _ := op_e2e_faultproofs.StartFaultDisputeSystem(t)
+	t.Cleanup(sys.Close)
+	disputeGameFactory := disputegame.NewAsteriscFactoryHelper(t, ctx, sys)
+	disputeGameFactory.StartChallenger(ctx, "Challenger",
+		challenger.WithAlphabet(sys.RollupEndpoint("sequencer")),
+		challenger.WithPrivKey(sys.Cfg.Secrets.Mallory))
+	preimageHelper := disputeGameFactory.PreimageHelper(ctx)
+	ident := preimageHelper.UploadLargePreimage(ctx, preimage.MinPreimageSize,
+		preimage.WithReplacedCommitment(10, common.Hash{0xaa}))
+
+	require.NotEqual(t, ident.Claimant, common.Address{})
+
+	preimageHelper.WaitForChallenged(ctx, ident)
+}
+
+func TestChallengeLargePreimages_ChallengeLast(t *testing.T) {
+	op_e2e.InitParallel(t)
+	ctx := context.Background()
+	sys, _ := op_e2e_faultproofs.StartFaultDisputeSystem(t)
+	t.Cleanup(sys.Close)
+	disputeGameFactory := disputegame.NewAsteriscFactoryHelper(t, ctx, sys)
+	disputeGameFactory.StartChallenger(ctx, "Challenger",
+		challenger.WithAlphabet(sys.RollupEndpoint("sequencer")),
+		challenger.WithPrivKey(sys.Cfg.Secrets.Mallory))
+	preimageHelper := disputeGameFactory.PreimageHelper(ctx)
+	ident := preimageHelper.UploadLargePreimage(ctx, preimage.MinPreimageSize,
+		preimage.WithLastCommitment(common.Hash{0xaa}))
+
+	require.NotEqual(t, ident.Claimant, common.Address{})
+
+	preimageHelper.WaitForChallenged(ctx, ident)
+}
