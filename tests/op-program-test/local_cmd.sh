8c3d087e5415dfd4f70c817a32c215d31e049253
pcw109550 <pcw109550@gmail.com>
2024-06-11 14:17:41 -0400

    Update op-program ci artifacts

--- tests/op-program-test/local_cmd.sh
+++ tests/op-program-test/local_cmd.sh
@@ -10,11 +10,11 @@
   --l2.genesis ./chain-artifacts/genesis-l2.json \
   --l1.trustrpc \
   --l1.rpckind debug_geth \
-  --l1.head 0xce8f502bb58452ab4597292b354949016f65b255030d044f15c2290dcbf15b96 \
-  --l2.head 0x721f4eb085f68959c0eb7df26cc7b6ec55873e8c8f0c987c09731a2d0fe9d774 \
-  --l2.outputroot 0xa4d328501f15ad607dd796aede9d71c2124b7d6985a9f960a0333b2939e538a2 \
-  --l2.claim 0x821c4841a99b02a501d2a6803f19737ee47039acbeb54b073f23bb3389bf075a \
-  --l2.blocknumber 20 \
+  --l1.head 0xd08fb3fa90d28261be12ad0863bd3268baf8629066901bd5b75a2a49f0c034a0 \
+  --l2.head 0xee0ccbbd6958805c7db525504eb3c5da3d8b467553fd6431996f72cef9397164 \
+  --l2.outputroot 0x4c746d0a08fffc6652dbb8800daddb058dd10f5a145c6fa651d301d9b72a20d8 \
+  --l2.claim 0xde14084b8fc8cfbffc5038648e0b7da3514faa716b9d1dc0dc040bd48bc17c35 \
+  --l2.blocknumber 8 \
   --datadir ./preimages \
   --log.format terminal \
   --server
