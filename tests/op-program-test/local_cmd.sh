311b533a5323956dab487dbe6080bcbc241196fb
Tei Im <tei.im@testinprod.io>
2024-06-20 15:02:35 -0600

    Update op-program-test

--- tests/op-program-test/local_cmd.sh
+++ tests/op-program-test/local_cmd.sh
@@ -10,11 +10,11 @@
   --l2.genesis ./chain-artifacts/genesis-l2.json \
   --l1.trustrpc \
   --l1.rpckind debug_geth \
-  --l1.head 0xd08fb3fa90d28261be12ad0863bd3268baf8629066901bd5b75a2a49f0c034a0 \
-  --l2.head 0xee0ccbbd6958805c7db525504eb3c5da3d8b467553fd6431996f72cef9397164 \
-  --l2.outputroot 0x4c746d0a08fffc6652dbb8800daddb058dd10f5a145c6fa651d301d9b72a20d8 \
-  --l2.claim 0xde14084b8fc8cfbffc5038648e0b7da3514faa716b9d1dc0dc040bd48bc17c35 \
-  --l2.blocknumber 8 \
+  --l1.head 0x52a2f6b4e022b51f00c262926f43383a216a957c3c3b9463b15866663da8bf44 \
+  --l2.head 0x4f0fe0525f90bf9012628a1dd1213b24751862005aa78d6b417897c56f88e440 \
+  --l2.outputroot 0x6035a4f175d1e45fded771eec8306357ccabe37de26abcdb51711395543305a4 \
+  --l2.claim 0x2d2db457102d783867103b7e7e6e1dce9e5bd6465f44ecd5e870c721b93be429 \
+  --l2.blocknumber 13 \
   --datadir ./preimages \
   --log.format terminal \
   --server
