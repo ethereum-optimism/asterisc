b5c2e0489a50b1c1c2de5d8bfde3ea556177ce29
Minhyuk Kim <kimminhyuk1004@gmail.com>
2024-07-28 17:33:29 +0900

    Refactor hashmap memory representation to radix-trie based memory

--- rvgo/fast/memory_test.go
+++ rvgo/fast/memory_test.go
@@ -24,14 +24,14 @@ func TestMemoryMerkleProof(t *testing.T) {
 	})
 	t.Run("fuller tree", func(t *testing.T) {
 		m := NewMemory()
-		m.SetUnaligned(0x10000, []byte{0xaa, 0xbb, 0xcc, 0xdd})
-		m.SetUnaligned(0x80004, []byte{42})
-		m.SetUnaligned(0x13370000, []byte{123})
+		m.SetUnaligned(0x1002221234200, []byte{0xaa, 0xbb, 0xcc, 0xdd})
+		m.SetUnaligned(0x8002212342204, []byte{42})
+		m.SetUnaligned(0x1337022212342000, []byte{123})
 		root := m.MerkleRoot()
-		proof := m.MerkleProof(0x80004)
+		proof := m.MerkleProof(0x8002212342204)
 		require.Equal(t, uint32(42<<24), binary.BigEndian.Uint32(proof[4:8]))
 		node := *(*[32]byte)(proof[:32])
-		path := uint32(0x80004) >> 5
+		path := 0x8002212342204 >> 5
 		for i := 32; i < len(proof); i += 32 {
 			sib := *(*[32]byte)(proof[i : i+32])
 			if path&1 != 0 {
@@ -77,28 +77,31 @@ func TestMemoryMerkleRoot(t *testing.T) {
 		root := m.MerkleRoot()
 		require.Equal(t, zeroHashes[64-5], root, "zero still")
 	})
-	t.Run("random few pages", func(t *testing.T) {
-		m := NewMemory()
-		m.SetUnaligned(PageSize*3, []byte{1})
-		m.SetUnaligned(PageSize*5, []byte{42})
-		m.SetUnaligned(PageSize*6, []byte{123})
-		p3 := m.MerkleizeSubtree((1 << PageKeySize) | 3)
-		p5 := m.MerkleizeSubtree((1 << PageKeySize) | 5)
-		p6 := m.MerkleizeSubtree((1 << PageKeySize) | 6)
-		z := zeroHashes[PageAddrSize-5]
-		r1 := HashPair(
-			HashPair(
-				HashPair(z, z),  // 0,1
-				HashPair(z, p3), // 2,3
-			),
-			HashPair(
-				HashPair(z, p5), // 4,5
-				HashPair(p6, z), // 6,7
-			),
-		)
-		r2 := m.MerkleizeSubtree(1 << (PageKeySize - 3))
-		require.Equal(t, r1, r2, "expecting manual page combination to match subtree merkle func")
-	})
+
+	//t.Run("random few pages", func(t *testing.T) {
+	//	m := NewMemory()
+	//	m.SetUnaligned(PageSize*3, []byte{1})
+	//	m.SetUnaligned(PageSize*5, []byte{42})
+	//	m.SetUnaligned(PageSize*6, []byte{123})
+	//	p3 := m.MerkleizeNode(m.radix, (1<<PageKeySize)|3, 0)
+	//	p5 := m.MerkleizeNode(m.radix, (1<<PageKeySize)|5, 0)
+	//	p6 := m.MerkleizeNode(m.radix, (1<<PageKeySize)|6, 0)
+	//	z := zeroHashes[PageAddrSize-5]
+	//	r1 := HashPair(
+	//		HashPair(
+	//			HashPair(z, z),  // 0,1
+	//			HashPair(z, p3), // 2,3
+	//		),
+	//		HashPair(
+	//			HashPair(z, p5), // 4,5
+	//			HashPair(p6, z), // 6,7
+	//		),
+	//	)
+	//	r2 := m.MerkleizeNode(m.radix, 1<<(PageKeySize-3), 0)
+	//	r3 := m.MerkleizeNode3(m.radix, 1, 0)
+	//	require.Equal(t, r1, r2, "expecting manual page combination to match subtree merkle func")
+	//	require.Equal(t, r3, r2, "expecting manual page combination to match subtree merkle func")
+	//})
 	t.Run("invalidate page", func(t *testing.T) {
 		m := NewMemory()
 		m.SetUnaligned(0xF000, []byte{0})
---
+++ rvgo/fast/radix.go
@@ -0,0 +1,486 @@
+package fast
+
+import (
+	"math/bits"
+)
+
+const (
+	// Define branching factors for each level
+	BF1 = 10
+	BF2 = 10
+	BF3 = 10
+	BF4 = 10
+	BF5 = 12
+)
+
+type RadixNode interface {
+	merkleize(m *Memory, addr, gindex uint64) [32]byte
+	//getChild(index uint64) RadixNode
+	//setChild(index uint64, child RadixNode)
+	invalidateHashes(branch uint64)
+}
+
+//func (n *baseRadixNode) invalidateHashes(branch uint64) {
+//	for index := branch + (1 << 10); index > 0; index /= 2 {
+//		n.HashCache[index] = false
+//		n.Hashes[index] = [32]byte{}
+//	}
+//}
+
+type RadixNodeLevel1 struct {
+	Children  [1 << BF1]*RadixNodeLevel2
+	Hashes    [2 * 1 << BF1][32]byte
+	HashCache [2 * 1 << BF1]bool
+}
+
+type RadixNodeLevel2 struct {
+	Children  [1 << BF2]*RadixNodeLevel3
+	Hashes    [2 * 1 << BF2][32]byte
+	HashCache [2 * 1 << BF2]bool
+}
+
+type RadixNodeLevel3 struct {
+	Children  [1 << BF3]*RadixNodeLevel4
+	Hashes    [2 * 1 << BF3][32]byte
+	HashCache [2 * 1 << BF3]bool
+}
+
+type RadixNodeLevel4 struct {
+	Children  [1 << BF4]*RadixNodeLevel5
+	Hashes    [2 * 1 << BF4][32]byte
+	HashCache [2 * 1 << BF4]bool
+}
+
+type RadixNodeLevel5 struct {
+	Hashes    [2 * 1 << BF5][32]byte
+	HashCache [2 * 1 << BF5]bool
+}
+
+func (n *RadixNodeLevel1) invalidateHashes(branch uint64) {
+	for index := branch + (1 << BF1); index > 0; index /= 2 {
+		n.HashCache[index] = false
+		n.Hashes[index] = [32]byte{}
+	}
+}
+func (n *RadixNodeLevel2) invalidateHashes(branch uint64) {
+	for index := branch + (1 << BF2); index > 0; index /= 2 {
+		n.HashCache[index] = false
+		n.Hashes[index] = [32]byte{}
+	}
+}
+func (n *RadixNodeLevel3) invalidateHashes(branch uint64) {
+	for index := branch + (1 << BF3); index > 0; index /= 2 {
+		n.HashCache[index] = false
+		n.Hashes[index] = [32]byte{}
+	}
+}
+func (n *RadixNodeLevel4) invalidateHashes(branch uint64) {
+	for index := branch + (1 << BF4); index > 0; index /= 2 {
+		n.HashCache[index] = false
+		n.Hashes[index] = [32]byte{}
+	}
+}
+
+func (n *RadixNodeLevel5) invalidateHashes(branch uint64) {
+	for index := branch + (1 << BF5); index > 0; index /= 2 {
+		n.HashCache[index] = false
+		n.Hashes[index] = [32]byte{}
+	}
+}
+
+func (m *Memory) Invalidate(addr uint64) {
+	// find page, and invalidate addr within it
+	if p, ok := m.pageLookup(addr >> PageAddrSize); ok {
+		prevValid := p.Ok[1]
+		p.Invalidate(addr & PageAddrMask)
+		if !prevValid { // if the page was already invalid before, then nodes to mem-root will also still be.
+			return
+		}
+	} else { // no page? nothing to invalidate
+		return
+	}
+
+	branchPaths := m.addressToBranchPath(addr)
+
+	currentLevel1 := m.radix
+
+	currentLevel1.invalidateHashes(branchPaths[0])
+	if currentLevel1.Children[branchPaths[0]] == nil {
+		return
+	}
+
+	currentLevel2 := currentLevel1.Children[branchPaths[0]]
+	currentLevel2.invalidateHashes(branchPaths[1])
+	if currentLevel2.Children[branchPaths[1]] == nil {
+		return
+	}
+
+	currentLevel3 := currentLevel2.Children[branchPaths[1]]
+	currentLevel3.invalidateHashes(branchPaths[2])
+	if currentLevel3.Children[branchPaths[2]] == nil {
+		return
+	}
+
+	currentLevel4 := currentLevel3.Children[branchPaths[2]]
+	currentLevel4.invalidateHashes(branchPaths[3])
+	if currentLevel4.Children[branchPaths[3]] == nil {
+		return
+	}
+
+	currentLevel5 := currentLevel4.Children[branchPaths[3]]
+	currentLevel5.invalidateHashes(branchPaths[4])
+}
+
+func (m *Memory) MerkleizeNodeLevel1(node *RadixNodeLevel1, addr, gindex uint64) [32]byte {
+	if gindex > 2*1<<BF1-1 {
+		return [32]byte{}
+	}
+
+	depth := uint64(bits.Len64(gindex))
+
+	if node.HashCache[gindex] {
+		if node.Hashes[gindex] == [32]byte{} {
+			return zeroHashes[64-5+1-depth]
+		} else {
+			return node.Hashes[gindex]
+		}
+	}
+
+	if gindex < 1<<BF1 {
+		left := m.MerkleizeNodeLevel1(node, addr, gindex<<1)
+		right := m.MerkleizeNodeLevel1(node, addr, (gindex<<1)|1)
+
+		r := HashPair(left, right)
+		node.Hashes[gindex] = r
+		node.HashCache[gindex] = true
+		return r
+	}
+
+	childIndex := gindex - 1<<BF1
+	if node.Children[childIndex] == nil {
+		return zeroHashes[64-5+1-depth]
+	}
+	addr <<= BF1
+	addr |= childIndex
+	return m.MerkleizeNodeLevel2(node.Children[childIndex], addr, 1)
+}
+
+func (m *Memory) MerkleizeNodeLevel2(node *RadixNodeLevel2, addr, gindex uint64) [32]byte {
+	if gindex > 2*1<<BF2 {
+		return [32]byte{}
+	}
+
+	depth := uint64(bits.Len64(gindex))
+
+	if node.HashCache[gindex] {
+		if node.Hashes[gindex] == [32]byte{} {
+			return zeroHashes[64-5+1-depth]
+		} else {
+			return node.Hashes[gindex]
+		}
+	}
+
+	if gindex < 1<<BF2 {
+		left := m.MerkleizeNodeLevel2(node, addr, gindex<<1)
+		right := m.MerkleizeNodeLevel2(node, addr, (gindex<<1)|1)
+
+		r := HashPair(left, right)
+		node.Hashes[gindex] = r
+		node.HashCache[gindex] = true
+		return r
+	}
+
+	childIndex := gindex - 1<<BF2
+	if node.Children[childIndex] == nil {
+		return zeroHashes[64-5+1-(depth+BF1)]
+	}
+
+	addr <<= BF2
+	addr |= childIndex
+	return m.MerkleizeNodeLevel3(node.Children[childIndex], addr, 1)
+}
+func (m *Memory) MerkleizeNodeLevel3(node *RadixNodeLevel3, addr, gindex uint64) [32]byte {
+	if gindex > 2*1<<BF3 {
+		return [32]byte{}
+	}
+
+	depth := uint64(bits.Len64(gindex))
+
+	if node.HashCache[gindex] {
+		if node.Hashes[gindex] == [32]byte{} {
+			return zeroHashes[64-5+1-depth]
+		} else {
+			return node.Hashes[gindex]
+		}
+	}
+
+	if gindex < 1<<BF3 {
+		left := m.MerkleizeNodeLevel3(node, addr, gindex<<1)
+		right := m.MerkleizeNodeLevel3(node, addr, (gindex<<1)|1)
+		r := HashPair(left, right)
+		node.Hashes[gindex] = r
+		node.HashCache[gindex] = true
+		return r
+	}
+
+	childIndex := gindex - 1<<BF3
+	if node.Children[childIndex] == nil {
+		return zeroHashes[64-5+1-(depth+BF1+BF2)]
+	}
+
+	addr <<= BF3
+	addr |= childIndex
+	return m.MerkleizeNodeLevel4(node.Children[childIndex], addr, 1)
+}
+
+func (m *Memory) MerkleizeNodeLevel4(node *RadixNodeLevel4, addr, gindex uint64) [32]byte {
+	if gindex > 2*1<<BF4 {
+		return [32]byte{}
+	}
+
+	depth := uint64(bits.Len64(gindex))
+
+	if node.HashCache[gindex] {
+		if node.Hashes[gindex] == [32]byte{} {
+			return zeroHashes[64-5+1-depth]
+		} else {
+			return node.Hashes[gindex]
+		}
+	}
+
+	if gindex < 1<<BF4 {
+		left := m.MerkleizeNodeLevel4(node, addr, gindex<<1)
+		right := m.MerkleizeNodeLevel4(node, addr, (gindex<<1)|1)
+
+		r := HashPair(left, right)
+		node.Hashes[gindex] = r
+		node.HashCache[gindex] = true
+		return r
+	}
+
+	childIndex := gindex - 1<<BF4
+	if node.Children[childIndex] == nil {
+		return zeroHashes[64-5+1-(depth+BF1+BF2+BF3)]
+	}
+
+	addr <<= BF4
+	addr |= childIndex
+	return m.MerkleizeNodeLevel5(node.Children[childIndex], addr, 1)
+}
+
+func (m *Memory) MerkleizeNodeLevel5(node *RadixNodeLevel5, addr, gindex uint64) [32]byte {
+	depth := uint64(bits.Len64(gindex))
+
+	if gindex >= (1 << BF5) {
+		pageIndex := (addr << BF5) | (gindex - (1 << BF5))
+		if p, ok := m.pages[pageIndex]; ok {
+			return p.MerkleRoot()
+		} else {
+			return zeroHashes[64-5+1-(depth+40)]
+		}
+	}
+
+	if node.HashCache[gindex] {
+		if node.Hashes[gindex] == [32]byte{} {
+			return zeroHashes[64-5+1-depth]
+		} else {
+			return node.Hashes[gindex]
+		}
+	}
+
+	left := m.MerkleizeNodeLevel5(node, addr, gindex<<1)
+	right := m.MerkleizeNodeLevel5(node, addr, (gindex<<1)|1)
+	r := HashPair(left, right)
+	node.Hashes[gindex] = r
+	node.HashCache[gindex] = true
+	return r
+
+}
+
+func (m *Memory) GenerateProof1(node *RadixNodeLevel1, addr, target uint64) [][32]byte {
+	var proofs [][32]byte
+
+	for idx := target + 1<<BF1; idx > 1; idx /= 2 {
+		sibling := idx ^ 1
+		proofs = append(proofs, m.MerkleizeNodeLevel1(node, addr, sibling))
+	}
+
+	return proofs
+}
+
+func (m *Memory) GenerateProof2(node *RadixNodeLevel2, addr, target uint64) [][32]byte {
+	var proofs [][32]byte
+
+	for idx := target + 1<<BF2; idx > 1; idx /= 2 {
+		sibling := idx ^ 1
+		proofs = append(proofs, m.MerkleizeNodeLevel2(node, addr, sibling))
+	}
+
+	return proofs
+}
+
+func (m *Memory) GenerateProof3(node *RadixNodeLevel3, addr, target uint64) [][32]byte {
+	var proofs [][32]byte
+
+	for idx := target + 1<<BF3; idx > 1; idx /= 2 {
+		sibling := idx ^ 1
+		proofs = append(proofs, m.MerkleizeNodeLevel3(node, addr, sibling))
+	}
+
+	return proofs
+}
+func (m *Memory) GenerateProof4(node *RadixNodeLevel4, addr, target uint64) [][32]byte {
+	var proofs [][32]byte
+
+	for idx := target + 1<<BF4; idx > 1; idx /= 2 {
+		sibling := idx ^ 1
+		proofs = append(proofs, m.MerkleizeNodeLevel4(node, addr, sibling))
+	}
+
+	return proofs
+}
+
+func (m *Memory) GenerateProof5(node *RadixNodeLevel5, addr, target uint64) [][32]byte {
+	var proofs [][32]byte
+
+	for idx := target + 1<<BF5; idx > 1; idx /= 2 {
+		sibling := idx ^ 1
+		proofs = append(proofs, m.MerkleizeNodeLevel5(node, addr, sibling))
+	}
+
+	return proofs
+}
+func (m *Memory) MerkleProof(addr uint64) (out [ProofLen * 32]byte) {
+	var proofs [][32]byte
+
+	branchPaths := m.addressToBranchPath(addr)
+
+	currentLevel1 := m.radix
+	branch1 := branchPaths[0]
+
+	proofs = m.GenerateProof1(currentLevel1, 0, branch1)
+
+	if currentLevel1.Children[branch1] == nil {
+		// append proofs
+	}
+
+	currentLevel2 := currentLevel1.Children[branch1]
+	branch2 := branchPaths[1]
+	//addr = branch1
+	proofs = append(m.GenerateProof2(currentLevel2, addr>>(pageKeySize-BF1), branch2), proofs...)
+
+	if currentLevel2.Children[branch2] == nil {
+		return
+	}
+
+	currentLevel3 := currentLevel2.Children[branch2]
+	branch3 := branchPaths[2]
+	//addr >>= BF2
+	//addr |= branch2
+	proofs = append(m.GenerateProof3(currentLevel3, addr>>(pageKeySize-BF1-BF2), branch3), proofs...)
+
+	if currentLevel3.Children[branch3] == nil {
+		return
+	}
+
+	currentLevel4 := currentLevel3.Children[branch3]
+	branch4 := branchPaths[3]
+	//addr >>= BF3
+	//addr |= branch3
+	proofs = append(m.GenerateProof4(currentLevel4, addr>>(pageKeySize-BF1-BF2-BF3), branch4), proofs...)
+
+	if currentLevel4.Children[branch4] == nil {
+		return
+	}
+
+	currentLevel5 := currentLevel4.Children[branch4]
+	branch5 := branchPaths[4]
+	//addr >>= BF4
+	//addr |= branch4
+	proofs = append(m.GenerateProof5(currentLevel5, addr>>(pageKeySize-BF1-BF2-BF3-BF4), branch5), proofs...)
+
+	//addr |= branch5
+	var subproofs [][32]byte
+	pageGindex := PageSize>>5 + (addr&PageAddrMask)>>5 //(1 << 7) | (addr & ((1 << 7) - 1))
+
+	for idx := pageGindex; idx > 1; idx /= 2 {
+		sibling := idx ^ 1
+		if p, ok := m.pages[addr>>PageAddrSize]; ok {
+			subproofs = append(subproofs, p.MerkleizeSubtree(uint64(sibling)))
+		} else {
+			subproofs = append(subproofs, zeroHashes[64-5+1-idx])
+		}
+
+	}
+	proofs = append(subproofs, proofs...)
+
+	if p, ok := m.pages[addr>>PageAddrSize]; ok {
+		proofs = append([][32]byte{p.MerkleizeSubtree(pageGindex)}, proofs...)
+	}
+	//
+	//for idx, proof := range proofs {
+	//	print(idx)
+	//	print(" : ")
+	//	print(string(proof[:]))
+	//	print("\n")
+	//}
+
+	// encode the proof
+	for i := 0; i < ProofLen; i++ {
+		copy(out[i*32:(i+1)*32], proofs[i][:])
+	}
+
+	return out
+}
+
+func (m *Memory) MerkleRoot() [32]byte {
+	return m.MerkleizeNodeLevel1(m.radix, 0, 1)
+}
+
+func (m *Memory) addressToBranchPath(addr uint64) []uint64 {
+	addr >>= PageAddrSize
+
+	path := make([]uint64, len(m.branchFactors))
+	for i := len(m.branchFactors) - 1; i >= 0; i-- {
+		bits := m.branchFactors[i]
+		mask := (1 << bits) - 1       // Create a mask for the current segment
+		path[i] = addr & uint64(mask) // Extract the segment using the mask
+		addr >>= bits                 // Shift the gindex to the right by the number of bits processed
+	}
+	return path
+}
+
+func (m *Memory) AllocPage(pageIndex uint64) *CachedPage {
+	p := &CachedPage{Data: new(Page)}
+	m.pages[pageIndex] = p
+
+	branchPaths := m.addressToBranchPath(pageIndex << PageAddrSize)
+
+	currentLevel1 := m.radix
+	branch1 := branchPaths[0]
+	if currentLevel1.Children[branch1] == nil {
+		currentLevel1.Children[branch1] = &RadixNodeLevel2{}
+	}
+	currentLevel2 := currentLevel1.Children[branch1]
+
+	branch2 := branchPaths[1]
+	if currentLevel2.Children[branch2] == nil {
+		currentLevel2.Children[branch2] = &RadixNodeLevel3{}
+	}
+	currentLevel3 := currentLevel2.Children[branch2]
+
+	branch3 := branchPaths[2]
+	if currentLevel3.Children[branch3] == nil {
+		currentLevel3.Children[branch3] = &RadixNodeLevel4{}
+	}
+	currentLevel4 := currentLevel3.Children[branch3]
+
+	branch4 := branchPaths[3]
+	if currentLevel4.Children[branch4] == nil {
+		currentLevel4.Children[branch4] = &RadixNodeLevel5{}
+	}
+
+	// For Level 5, we don't need to allocate a child node
+
+	return p
+}
